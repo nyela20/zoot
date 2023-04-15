067a82df505fe1a1782e4f0e9283bf608cd6754d
ralanton1u <ny-elanirina-n.ralantonisainana1@etu.univ-lorraine.fr>
2023-04-15 10:22:30 +0200

    Zoot4 Ajout inegalite binaire

--- src/zoot/arbre/declaration/EgaliteBinaire.java
+++ src/zoot/arbre/declaration/EgaliteBinaire.java
@@ -1,18 +1,45 @@
 package zoot.arbre.declaration;
 
+import zoot.exceptions.AnalyseSemantiqueException;
+
 public class EgaliteBinaire extends Binaire{
+    private String etiquette1;
+    private String etiquette2;
 
     public EgaliteBinaire(Expression opGauche, Expression opDroite, int n) {
         super(opGauche, opDroite, n);
     }
 
     @Override
+    public void verifier(){
+        super.verifier();
+        if(operandeDroite.getSymbole() == Type.UNDEFINED) return ;
+        if(operandeDroite.getSymbole() == Type.UNDEFINED) return ;
+        if(operandeDroite.getSymbole() != operandeGauche.getSymbole()) AnalyseSemantiqueException.raiseAnalyseSemantiqueException(noLigne, "Expression de type differents !");
+    }
+
+    @Override
     public String toMIPS() {
-        return null;
+        StringBuilder tomips = new StringBuilder();
+        this.etiquette1 = TDS.getInstance().generationNouvelleEtiquette();
+        this.etiquette2 = TDS.getInstance().generationNouvelleEtiquette();
+        tomips.append("\t#Operante gauche\n");
+        tomips.append(operandeGauche.toMIPS());
+        tomips.append("\tsw $v0, ($sp) \t #Empilement\n");
+        tomips.append("\taddi $sp, $sp, -4\n");
+        tomips.append("\t#Operante droite\n");
+        tomips.append(operandeDroite.toMIPS());
+        tomips.append("\tbeq $v1, $v0, " + etiquette1 + "\n");
+        tomips.append("\tli $v0, 0\n");
+        tomips.append("\tj " + etiquette2 + "\n");
+        tomips.append(etiquette1 + ":\n");
+        tomips.append("\tli $v0, 1\n");
+        tomips.append(etiquette2 + ":\n");
+       return tomips.toString();
     }
 
     @Override
     public Type getSymbole() {
-        return null;
+        return Expression.Type.BOOLEEN;
     }
 }
---
+++ src/zoot/arbre/declaration/InegaliteBinaire.java
@@ -0,0 +1,4 @@
+package zoot.arbre.declaration;
+
+public class InegaliteBinaire {
+}
